01b9c26967641ece49b5c5c89d1406863a39ac25
Veljko <veljkoklincov99@gmail.com>
2023-08-23 23:59:36 +0200

    Classes created

---
+++ Server/Common/Audit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public enum MESSAGETYPE { INFO, ERROR};
+    public class Audit
+    {
+        public int Id { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public MESSAGETYPE MessageType { get; set; }
+        
+        public string Message { get; set; }
+
+    }
+}
---
+++ Server/Common/Load.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Common
+{
+    public class Load
+    {
+        public int Id { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public double ForecastValue { get; set; }
+        public double MeasuredValue { get; set; }
+
+
+    }
+}
