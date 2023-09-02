7258ce41508df569af8f41be8fa2a6a69dc72189
Veljko <veljkoklincov99@gmail.com>
2023-09-03 00:43:31 +0200

    Final Commit

--- Server/DataBase/DatabaseService.cs
+++ Server/DataBase/DatabaseService.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Xml;
 using System.Xml.Linq;
 using System.Xml.Serialization;
@@ -14,22 +13,26 @@ namespace DataBase
 
     public class DatabaseService : IRead, IWrite
     {
+        public static Dictionary<int, Load> InMemory = new Dictionary<int, Load>();
         private static int id = 1;
+        public delegate void DeleteInMemory();
+        //public event DeleteInMemory DeleteMemory;
+        //DeleteMemory+=DeleteMem
+
+
         public List<Load> ReadingCsvFile(MemoryStream csv, List<string> ucitani)
         {
-           
+            Dictionary<int, Load>temp=new Dictionary<int, Load>();
             List<Load> loads = new List<Load>();
-
+            List<Load> FromInMemory = new List<Load>();
             int line = 1;
 
-            DateTime currentTime=DateTime.Now;
-            TimeSpan dataTimeout=TimeSpan.FromMinutes(15);
 
             using (StreamReader csvStream = new StreamReader(csv))
             {
                 string csvData = csvStream.ReadToEnd();
                 string[] csvRows = csvData.Split('\n');
-                Dictionary<int, Load> map = new Dictionary<int, Load>();
+                bool postoji=false;
                 // Preskačemo prvi red kako bismo izbegli zaglavlje (naslovnu liniju).
                 // Koristimo i Length - 1 kako bismo izbegli poslednji red koji može biti prazan.
                 string[] rows = csvRows.Skip(1).Take(csvRows.Length - 2).ToArray();
@@ -45,31 +48,86 @@ namespace DataBase
                             double.TryParse(split[2], out double forecastValue) &&
                             double.TryParse(split[3], out double measuredValue))
                         {
-                            foreach(var s in ucitani)
+                            if (InMemory.Count != 0)
                             {
-                                if (split[0].Replace('-', '_').Contains(s))
+                                foreach (var kvp in InMemory)
                                 {
+                                    Load l = new Load();
+                                    l = kvp.Value;
+
+                                    if (l.Timestamp.ToString("yyyy-MM-dd").Contains(timestamp.ToString("yyyy-MM-dd")))
+                                    {
+                                        FromInMemory.Add(l);
+                                        postoji = true;
+                                    }
                                     
-                                    return ReadFromXML(s);
+                                }
+                                if (postoji)
+                                {
+                                    return FromInMemory;
+                                }
+
+                            }
+                            if (CheckXML(split[0]))
+                            {
+                                foreach (var s in ucitani)
+                                {
+                                    if (split[0].Replace('-', '_').Contains(s))
+                                    {
+                                        id = 1;
+                                        InMemory.Add(line, new Load(line, timestamp, forecastValue, measuredValue));
+                                        return ReadFromXML(s);
+                                    }
                                 }
                             }
                             // Ako su svi podaci ispravno parsirani, kreiramo Load objekat i dodajemo ga u listu.
                             Load load = new Load(line, timestamp, forecastValue, measuredValue);
+                            temp.Add(line, load);
                             loads.Add(load);
-                            
-                            
+
+
                         }
-                        
+
                     }
-                    
+
 
                     line++; // Povećavamo brojač linija.
                 }
             }
-            Audit a1=(new Audit(line, DateTime.Now, MESSAGETYPE.INFO, "Podaci uspesno procitani i prosledjeni"));
+            foreach(var kvp in temp)
+            {
+                InMemory[kvp.Key] = kvp.Value;
+            }
+            //DeleteInMemory? Invoke(parametri);
+            Audit a1 = (new Audit(line, DateTime.Now, MESSAGETYPE.INFO, "Podaci uspesno procitani i prosledjeni"));
             string recvMessage = WriteInXML(loads, a1);
             return loads; // Vraćamo listu Load objekata.
         }
+        public bool CheckXML(string datum)
+        {
+            using (IImportedFile f = new DatabaseService().ReadFile(ConfigurationManager.AppSettings["DataBaseLoads"]))
+            {
+                XmlDocument xml = new XmlDocument();
+                xml.Load(((ImportedFile)f).MemoryStream);
+
+                XmlNodeList xmlLoads = xml.SelectNodes("//row[TIME_STAMP[contains(., '" + datum + "')]]");
+                foreach (XmlNode xmlNode in xmlLoads)
+                {
+                    DateTime date = DateTime.Parse(xmlNode.SelectSingleNode("TIME_STAMP").InnerText);
+                    if (datum == date.ToString("yyyy-MM-dd"))
+                    {
+                        return true;
+                    }
+
+                }
+
+            }
+            return false;
+        }
+        /*public void DeleteLoads(Dictionary<int, Load> loads)
+        {
+
+        }*/
         public string WriteInXML(List<Load> loads, Audit audit)
         {
             WriteAudit(audit, ConfigurationManager.AppSettings["DataBaseAudit"]);
@@ -126,16 +184,16 @@ namespace DataBase
             return "Izvrsen je upis objekata u TBL_LOAD.xml";
 
         }
-        public List<Load>ReadFromXML(string s)
+        public List<Load> ReadFromXML(string s)
         {
-            List<Load>loads= new List<Load>();
-            using (IImportedFile f=new DatabaseService().ReadFile(ConfigurationManager.AppSettings["DataBaseLoads"]))
+            List<Load> loads = new List<Load>();
+            using (IImportedFile f = new DatabaseService().ReadFile(ConfigurationManager.AppSettings["DataBaseLoads"]))
             {
                 XmlDocument xml = new XmlDocument();
                 xml.Load(((ImportedFile)f).MemoryStream);
                 string date = s.Replace('_', '-');
-                XmlNodeList xmlLoads = xml.SelectNodes("//row[TIME_STAMP[contains(., '" + date+"')]]");
-                foreach ( XmlNode xmlNode in xmlLoads)
+                XmlNodeList xmlLoads = xml.SelectNodes("//row[TIME_STAMP[contains(., '" + date + "')]]");
+                foreach (XmlNode xmlNode in xmlLoads)
                 {
                     Load load = new Load();
                     load.Id = id++;
@@ -144,7 +202,7 @@ namespace DataBase
                     load.MeasuredValue = double.Parse(xmlNode.SelectSingleNode("MEASURED_VALUE").InnerText);
                     loads.Add(load);
                 }
-               
+
 
             }
             return loads;
@@ -201,7 +259,7 @@ namespace DataBase
                     insertElement.Add(new XElement("ID", ++maxRows));
                     insertElement.Add(new XElement("TIME_STAMP", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")));
                     insertElement.Add(new XElement("MESSAGE_TYPE", "Error"));
-                    insertElement.Add(new XElement("MESSAGE", "U bazi podataka ne postoje podaci za datum "+audit.Message));
+                    insertElement.Add(new XElement("MESSAGE", "U bazi podataka ne postoje podaci za datum " + audit.Message));
 
                     items.Add(insertElement);
                     xmlf.Save(ConfigurationManager.AppSettings["DataBaseAudit"]);
@@ -250,7 +308,7 @@ namespace DataBase
         {
             ms.Position = 0;
             XmlSerializer serializer = new XmlSerializer(typeof(Audit));
-            Audit a1=(Audit)serializer.Deserialize(ms);
+            Audit a1 = (Audit)serializer.Deserialize(ms);
             WriteAudit(a1, ConfigurationManager.AppSettings["DataBaseAudit"]);
             return "Podaci su upisani u TBL_AUDIT.xml";
         }
